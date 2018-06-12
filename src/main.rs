fbe3e5d00fece332f29c1eb3d5352fe9d8c02300
Levi McCallum <levi@levimccallum.com>
2018-06-12 01:34:18 -0700

    Get it encoding a blank bitmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate bmp;
+
+use bmp::{Pixel, Image};
+
 fn main() {
-    println!("Hello, world!");
+    let red = Pixel::new(255, 0, 0);
+
+    let mut img = Image::new(100, 100);
+    img.set_pixel(52, 41, red);
+    let _ = img.save("output.bmp");
 }
