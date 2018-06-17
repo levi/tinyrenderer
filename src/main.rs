454113f42096b0fd25e85cbe61e18e6fdf81c50a
Levi McCallum <levi@levimccallum.com>
2018-06-16 22:07:47 -0700

    Better line drawing

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,53 @@ fn main() {
     let red = Pixel::new(255, 0, 0);
 
     let mut img = Image::new(100, 100);
-    img.set_pixel(52, 41, red);
-    img.draw_line(2.0, 20.0, 40.0, 55.0, white);
+
+    img.draw_line(13, 20, 80, 40, white); 
+    img.draw_line(20, 13, 40, 80, red); 
+    img.draw_line(80, 40, 13, 20, red);
+
     let _ = img.save("output.bmp");
 }
 
 trait ImageMethods {
-    fn draw_line(&mut self, x0: f32, y0: f32, x1: f32, y1: f32, pixel: Pixel);
+    fn draw_line(&mut self, x0: i32, y0: i32, x1: i32, y1: i32, pixel: Pixel);
 }
 
 impl ImageMethods for Image {
-    fn draw_line(&mut self, x0: f32, y0: f32, x1: f32, y1: f32, pixel: Pixel) {
-        let mut t = 0.0;
-        while t < 1.0 {
-            let x = x0 * (1.0 - t) + x1 * t;
-            let y = y0 * (1.0 - t) + y1 * t;
-            self.set_pixel(x as u32, y as u32, pixel);
-            t += 0.01;
+    fn draw_line(&mut self, x0: i32, y0: i32, x1: i32, y1: i32, pixel: Pixel) {
+        let mut x0 = x0;
+        let mut x1 = x1;
+        let mut y0 = y0;
+        let mut y1 = y1;
+
+        let steep = (y1 - y0).abs() > (x1 - x0).abs();
+        if steep {
+            std::mem::swap(&mut x0, &mut y0);
+            std::mem::swap(&mut x1, &mut y1);
+        }
+        if x0 > x1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }
+
+        let dx = x1 - x0;
+        let dy = y1 - y0;
+        let gradient = dy as f64 / dx as f64;
+        
+        let mut y = y0;
+        let mut e = gradient - 1.0;
+
+        for x in 0..x1 {
+            if steep {
+                self.set_pixel(y as u32, x as u32, pixel);
+            } else {
+                self.set_pixel(x as u32, y as u32, pixel);
+            }
+            if e >= 0.0 {
+                y += 1;
+                e -= 1.0;
+            }
+            e += gradient;
         }
     }   
 }
